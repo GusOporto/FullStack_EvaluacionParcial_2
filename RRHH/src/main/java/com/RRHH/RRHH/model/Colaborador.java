8c2613ba0114aca3c6937b97cf8c6fcd0b69ac4f
GusOporto <gu.oporto@duocuc.cl>
2026-05-10 20:53:22 -0400

    Implementacion relaciones Evaluciones y Curriculum
    Se implementan las relaciones ManyToOne desde Colaborador a Evaluaciones y OneToMany desde Colaborador a Curriculum

--- RRHH/src/main/java/com/RRHH/RRHH/model/Colaborador.java
+++ RRHH/src/main/java/com/RRHH/RRHH/model/Colaborador.java
@@ -14,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
@@ -84,4 +85,11 @@ public class Colaborador {
         @ManyToOne
         @JoinColumn(name = "cargos_id", nullable = false)
         private Cargos cargos;
+
+        @ManyToOne
+        @JoinColumn(name = "evaluaciones", nullable = false)
+        private Evaluaciones evaluaciones;
+
+        @OneToMany(mappedBy = "curriculum")
+        private List<Curriculum> curriculum;
 }
